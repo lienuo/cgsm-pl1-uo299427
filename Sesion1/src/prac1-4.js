cf25961f95d9e76ef60ed447b6faf56b52c1e2d5
lienuo <uo299427@uniovi.es>
2024-01-31 15:02:18 -0800

    figuras

--- Sesion1/src/prac1-4.js
+++ Sesion1/src/prac1-4.js
@@ -9,17 +9,24 @@ if ( WEBGL.isWebGLAvailable() ) {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
-    camera.position.set( 0, 0, 300 )
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
-    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
-    const cube = new THREE.Mesh( geometry, material ); 
-    scene.add( cube );
-    cube.rotation.set( Math.PI / 5, Math.PI / 5, 0 );
-    window.addEventListener( 'resize', ( ) => {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix( );
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.render( scene, camera );
-    }, false );
+    camera.position.set( 0, 0, 300 );
+    //Bok
+    const geometry = new THREE.BoxGeometry( 40, 40, 40 );
+    const material = new THREE.MeshBasicMaterial( );
+    const box = new THREE.Mesh( geometry, material );
+    //box.position.set(100,0,0)
+    box.position.set(-100,0,0)
+    //box.rotation.set( Math.PI / 5, Math.PI / 5, 0 );
+    //Cilindro.
+    const geometrycilindro = new THREE.CylinderGeometry( 10, 10, 40, 32 ); 
+    const material1 = new THREE.MeshBasicMaterial( {color: 0xffff00} ); 
+    const cylinder = new THREE.Mesh( geometrycilindro, material1 ); scene.add( cylinder )
+    //Esfera
+    const geometryesfera = new THREE.SphereGeometry( 15, 40, 40 ); 
+    const material2 = new THREE.MeshBasicMaterial( { color: 0xffff25 } ); 
+    const sphere = new THREE.Mesh( geometryesfera, material2 ); scene.add( sphere );
+    //sphere.position.set(-100,0,0)
+    sphere.position.set(100,0,0)
 
-}
+    scene.add( box );
+    renderer.render( scene, camera );}
--- Sesion1/webpack.config.js
+++ Sesion1/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
         "prac1-1": "./src/prac1-1.js",
         "prac1-2": "./src/prac1-2.js",
         "prac1-3": "./src/prac1-3.js",
-        "prac1-3": "./src/prac1-4.js",
+        "prac1-4": "./src/prac1-4.js"
     },
     devtool: 'inline-source-map',
     devServer: {
