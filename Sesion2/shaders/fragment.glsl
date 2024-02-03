d9a1899ea37e22f65e6f4e7ac2003e3d52d5ec6d
lienuo <uo299427@uniovi.es>
2024-02-03 07:20:21 -0800

    sol

---
+++ Sesion2/shaders/fragment.glsl
@@ -0,0 +1,82 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+uniform float time;
+
+uniform float fogDensity;
+uniform vec3 fogColor;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+varying vec2 vUv;
+
+void main( void ) {
+
+    vec2 position = - 1.0 + 2.0 * vUv;
+
+    vec4 noise = texture2D( texture1, vUv );
+    vec2 T1 = vUv + vec2( 1.5, - 1.5 ) * time * 0.02;
+    vec2 T2 = vUv + vec2( - 0.5, 2.0 ) * time * 0.01;
+
+    T1.x += noise.x * 2.0;
+    T1.y += noise.y * 2.0;
+    T2.x -= noise.y * 0.2;
+    T2.y += noise.z * 0.2;
+
+    float p = texture2D( texture1, T1 * 2.0 ).a;
+
+    vec4 color = texture2D( texture2, T2 * 2.0 );
+    vec4 temp = color * ( vec4( p, p, p, p ) * 2.0 ) + ( color * color - 0.1 );
+
+    if( temp.r > 1.0 ) { temp.bg += clamp( temp.r - 2.0, 0.0, 100.0 ); }
+    if( temp.g > 1.0 ) { temp.rb += temp.g - 1.0; }
+    if( temp.b > 1.0 ) { temp.rg += temp.b - 1.0; }
+
+    gl_FragColor = temp;
+
+    float depth = gl_FragCoord.z / gl_FragCoord.w;
+    const float LOG2 = 1.442695;
+    float fogFactor = exp2( - fogDensity * fogDensity * depth * depth * LOG2 );
+    fogFactor = 1.0 - clamp( fogFactor, 0.0, 1.0 );
+
+    gl_FragColor = mix( gl_FragColor, vec4( fogColor, gl_FragColor.w ), fogFactor );
+}
--- Sesion2/webpack.config.js
+++ Sesion2/webpack.config.js
@@ -7,7 +7,9 @@ module.exports = {
         "prac2-4": "./src/prac2-4.js",
         "prac2-5": "./src/prac2-5.js",
         "prac2-6": "./src/prac2-6.js",
-        "prac2-7": "./src/prac2-7.js"
+        "prac2-7": "./src/prac2-7.js",
+        "prac2-8": "./src/prac2-8.js"
+
 
    
     },
@@ -23,5 +25,15 @@ module.exports = {
     performance: {
         maxAssetSize: 1000000,
         maxEntrypointSize: 1000000
-    }
-};
+    },
+    module: {
+        rules: [
+          {
+            test: /\.glsl$/,
+            use: {
+              loader: 'webpack-glsl-loader'
+            }
+          }
+        ]
+      }
+    } ;
